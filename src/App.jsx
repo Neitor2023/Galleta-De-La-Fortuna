a8dfd7b9539cca70cb88447590fa1398836ef894
Romy Garcia <neitorme@gmail.com>
2023-02-25 10:36:47 -0500

    Vaciando archivo No. 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,10 @@
 // main
 import './App.css'
 import { useState } from 'react'
-import Message from './components/Message'
 
 function App() {
-  const [ isVisible, setIsVisible ] = useState (true)
-
-  const funcionDetonada = () =>{
-    setIsVisible(!isVisible)
-  }
-
-  // const [count, setCount] = useState(0)
-
     return (
     <div className="App">
-        {/* <Bombillo/> */}
-        <Message
-        visible={ isVisible }
-        dataSalida={ funcionDetonada }
-        />
-
-        <Message
-        visible={ isVisible }
-        dataSalida={ funcionDetonada }
-        />
         
     </div>
   )
