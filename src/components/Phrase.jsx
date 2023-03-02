1bd93fe6f4e46676bab2d001382ba43b586c6509
Romy Garcia <neitorme@gmail.com>
2023-03-01 23:51:55 -0500

    Autor

--- src/components/Phrase.jsx
+++ src/components/Phrase.jsx
@@ -2,6 +2,8 @@ const Phrase = ({phraseData}) => {
   return(
     <div className="frase">
       <h1>{ phraseData.phrase }</h1>
+      <br />
+      <h2>Autor: {phraseData.author} </h2>
     </div>
   )
 }
